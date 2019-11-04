01ed1422df32782932d18551cbdc6ec5052594ba
Nep T. Orshiso <nep.orshiso@gmail.com>
2019-11-04 16:33:32 -0500

    sending json when a route is requested

--- apple_ceos/routes/ceos.js
+++ apple_ceos/routes/ceos.js
@@ -7,16 +7,9 @@ router.get("/", async function(req, res, next) {
   // Request to our model. This is the controller accessing the model.  
   const executiveData = await ceoModel.getAll();
   
-  res.render("template", {
-        locals: {
-            title: "Apple CEOs page",
-            // This is the controller creating the view
-            data: executiveData
-        },
-        partials: {
-            partial: "partial-ceos"
-        }
-    });
+  // JSON API
+  res.status(200).json(executiveData)
+  
 });
 
 module.exports = router;
